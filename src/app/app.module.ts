aaafdd8411f742f0d8830c66761ddae66a42ba06
Ravi Shankar <ravioshankar@outlook.com>
2020-08-09 11:26:18 -0500

    update angular version to 10.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { CustomControlModule } from './../../projects/custom-control/src/lib/custom-control.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes, Router } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatSliderModule} from '@angular/material';
 import { OlProjSetupModule } from './../../projects/ol-proj-setup/src/public_api';
 import { BasicSetupModule } from './../../projects/basic-setup/src/public_api';
 import { CustomMarkersModule } from './../../projects/custom-markers/src/public_api';
 import { SimplePopupModule } from './../../projects/simple-popup/src/public_api';
 import { ClarityModule } from '@clr/angular';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { ClusteredFeaturesModule } from './../../projects/clustered-features/src/public-api';
 import { GeojsonRenderingModule } from './../../projects/geojson-rendering/src/public-api';
 import { HeaderComponent } from './header/header.component';
@@ -33,7 +29,6 @@ import { FooterComponent } from './footer/footer.component';
     AppRoutingModule,
     BasicSetupModule,
     BrowserAnimationsModule,
-    MatButtonModule,
     OlProjSetupModule,
     CustomMarkersModule,
     SimplePopupModule,
@@ -42,9 +37,7 @@ import { FooterComponent } from './footer/footer.component';
     ClarityModule,
     DocsModule,
     ClusteredFeaturesModule,
-    MatSliderModule,
-    GeojsonRenderingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    GeojsonRenderingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
